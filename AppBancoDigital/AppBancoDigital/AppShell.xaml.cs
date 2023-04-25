cc0bdd50815ba63ad5883b2a9c6bd4b63a36be05
Usuario <Usuario@CPD02-M05>
2023-04-25 14:33:14 -0300

    Adicionar arquivos de projeto.

---
+++ AppBancoDigital/AppBancoDigital/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppBancoDigital.ViewModels;
+using AppBancoDigital.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppBancoDigital
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
